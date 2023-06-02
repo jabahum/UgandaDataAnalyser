7d192fb990c2b2f0ac165b3a39dc8910afce662d
BruceMD <bruce.dickie@jembi.org>
2023-06-02 13:11:00 +0200

    Updated changes

--- src/api/routes/Patients.py
+++ src/api/routes/Patients.py
@@ -1,5 +1,8 @@
+from time import sleep
+
 import requests
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
+import json
 
 from src.api.utils import Responses
 from src.api.utils.Responses import response_with, SUCCESS_200, SERVER_ERROR_500
@@ -13,13 +16,37 @@ path = "&_sort=-_lastUpdated"
 
 
 @patient_routes.route("/", methods=["GET"])
-def get_patients():
+def get_patients(inputURL=None):
     try:
         args = request.args
         count = args.get("count")
         auth = HTTPBasicAuth('postman', 'password')
-        response = requests.get(DevelopmentConfig.SERVER_URL + count + path, auth=auth)
-        return response_with(Responses.SUCCESS_200, value={"patients": response.json()})
+        if inputURL:
+            response = requests.get(inputURL, auth=auth).json()
+        else:
+            response = requests.get(DevelopmentConfig.SERVER_URL + count + path, auth=auth).json()
+        # print(response['link'])
+
+        print(response)
+
+        url = ""
+        for item in response['link']:
+            if item['relation'] == 'self':
+                pass
+            elif item['relation'] == 'next':
+                url = item['url']
+                try:
+                    url = url.replace("localhost:8080", "100.66.44.100:7080")
+                    print(url)
+                except:
+                    print("Wrong format")
+                print("Sleep")
+                sleep(2)
+                get_patients(url)
+            else:
+                print("weird")
+
+        return response_with(Responses.SUCCESS_200, value={"patients": response})
     except Exception as e:
         print(e)
         return response_with(Responses.SERVER_ERROR_500)
